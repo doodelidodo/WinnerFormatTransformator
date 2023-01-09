1c0e24149fb73455d0a86dac8cb85268ea6d5194
doodelidodo <dominic.meyer@me.com>
2023-01-09 16:13:59 +0100

    v5
    refactoring
    - Error handling verbessert
    - Aufbearbeitung mit verschidenem Inhalt getestet und daran verbessert

--- katalogImport.py
+++ katalogImport.py
@@ -2,31 +2,48 @@ from util import *
 
 try: 
     import_file = get_file_from_folder()
+    print("File wurde gefunden")
+
+    try:
+        prefix = get_prefix(import_file)
+        print("Prefix wurde gefunden")
+
+        try:
+            new_catalog = import_csv()
+            print("Katalog erfolgreich importiert")
+
+            try:
+                new_catalog = transform_dataset(new_catalog, prefix)
+                print("Katalog erfolgreich transformiert")
+                try:
+                    export_import_files(new_catalog, prefix)
+                    print("Katalog ist bereit für den Import ins Abacus")
+                    success()
+                    print("ENDE")
+
+                except:
+                    error_handling("Der Katalog konnte nicht exportiert werden")
+            except:
+                error_handling("Der Katalog konnte nicht korrekt mutiert werden für den Import")
+
+        except:
+            error_handling("Der Katalog konnte nicht korrekt importiert werden")
+
+    except:
+        error_handling("Für den Katalog " + import_file + " wurde kein Prefix in der Config definiert")
+
 except:
     error_handling("Der Katalog konnte nicht auf den Server geladen werden", False)
 
 
-try:
-    prefix = get_prefix(import_file)
-except:
-    error_handling("Für den Katalog " + import_file + " wurde kein Prefix in der Config definiert")
 
 
-try: 
-    new_catalog = import_csv()
-except:
-    error_handling("Der Katalog konnte nicht korrekt importiert werden")
+
+
 
 
 # Die importierten Daten ins Abacus Format transformieren
-try:
-    new_catalog = transform_dataset(new_catalog, prefix)
-except:
-    error_handling("Der Katalog konnte nicht korrekt mutiert werden für den Import")
 
 
-try:
-    export_import_files(new_catalog, prefix)
-    success()
-except: 
-    error_handling("Der Katalog konnte nicht exportiert werden")
+
+
--- util.py
+++ util.py
@@ -1,13 +1,14 @@
+import codecs
+import time
+
 import pandas as pd
 import json
 from datetime import datetime
 import os
 import configparser
-import tkinter as tk
-from tkinter import messagebox
 import re
 
-CONFIG = "D:/Abacus/WinnerImport/WinnterTransformator.cfg"
+CONFIG = "D:/Abacus/WinnerImport/WinnerTransformator.cfg"
 
 parser = configparser.ConfigParser()
 parser.read(CONFIG)
@@ -25,11 +26,13 @@ def get_file_from_folder():
 
 
 def import_csv():
-    csv_data = pd.read_csv(IMPORT_FOLDER + get_file_from_folder(), sep = SEPARATOR, encoding = ENCODING, low_memory=False)
+    csv_data = pd.read_csv(IMPORT_FOLDER + get_file_from_folder(), sep=SEPARATOR, encoding=ENCODING, low_memory=False)
     return pd.DataFrame(csv_data)
 
 
 def transform_dataset(df, prefix):
+    df = df.astype({'!Artikel-Nr.': str})
+    df = df.iloc[1:]
     df = df.rename(columns={"!Artikel-Nr.": "ArtikelNr"})
     data = pricegroup_separator(df)
     data = set_serie(data)
@@ -37,8 +40,8 @@ def transform_dataset(df, prefix):
     data['ArtikelNr'] = data['ArtikelNr'].str.replace('#', '')
     data['ArtikelNr'] = prefix + "-" + data['ArtikelNr']
     data['prefix'] = prefix
-    data = data.assign(Preisgruppe = lambda dataframe: dataframe['Preisgruppe']
-    .map(lambda anr: anr.split(".")[0]))
+    data = data.assign(Preisgruppe=lambda dataframe: dataframe['Preisgruppe']
+                       .map(lambda anr: anr.split(".")[0]))
     return data
 
 
@@ -46,22 +49,23 @@ def pricegroup_separator(df):
     preisgruppen = []
     not_preisgruppen = []
     for prod in df.columns:
-        if 'Preisgruppe' in prod: 
+        if 'Preisgruppe' in prod:
             preisgruppen.append(prod)
-        else: 
+        else:
             not_preisgruppen.append(prod)
     data = df.melt(id_vars=not_preisgruppen, value_vars=preisgruppen, var_name='Preisgruppe', value_name='Preis')
     return data[data['Preis'].notnull()].sort_values(by=["ArtikelNr", "Preisgruppe"])
 
 
 def set_serie(df):
-    return df.assign(serie = lambda dataframe: dataframe['ArtikelNr'].map(lambda anr: anr.split("@@",1)[1] if '@@' in anr else ''))
+    return df.assign(
+        serie=lambda dataframe: dataframe['ArtikelNr'].map(lambda anr: anr.split("@@", 1)[1] if '@@' in anr else ''))
 
 
-def get_prefix(fileName):
-    the_pref= ""
-    for pref in PREFIXES: 
-        if get_label(fileName) == pref['FileName']:
+def get_prefix(file_name):
+    the_pref = ""
+    for pref in PREFIXES:
+        if get_label(file_name) == pref['FileName']:
             the_pref = pref['prefix']
     if the_pref == "":
         raise Exception("Kein Prefix gefunden")
@@ -71,42 +75,44 @@ def get_prefix(fileName):
 
 def export_import_files(df, prefix):
     all_products = mark_variants(df)
-    dt = dt = datetime.now().strftime("%Y%m%d-%H%M%S")
+    dt = datetime.now().strftime("%Y%m%d-%H%M%S")
     file_name = EXPORT_FOLDER + prefix + "-all-" + dt + ".xlsx"
-    all_products.to_excel(file_name, index=False) 
+    all_products.to_excel(file_name, index=False)
 
 
 def mark_variants(df):
-    singleProds = df['ArtikelNr'].unique()
+    single_prods = df['ArtikelNr'].unique()
     varianten = pd.DataFrame()
     non_varianten = pd.DataFrame()
-    for prod in singleProds:
-        filteredProds = df[df.ArtikelNr == prod]
-        if len(filteredProds['ArtikelNr']) > 1:
-            varianten = pd.concat([varianten, filteredProds])
-        else: 
-            non_varianten = pd.concat([non_varianten, filteredProds])
+    for prod in single_prods:
+        filtered_prods = df[df.ArtikelNr == prod]
+        if len(filtered_prods['ArtikelNr']) > 1:
+            varianten = pd.concat([varianten, filtered_prods])
+        else:
+            non_varianten = pd.concat([non_varianten, filtered_prods])
     varianten['variante'] = 1
     non_varianten['variante'] = 0
     return pd.concat([varianten, non_varianten])
 
 
-def error_handling(error, delete = True):
-    with open(ERROR_FOLDER + "error.txt", "a") as f:
-        f.write(error)
-    messagebox.showerror("Da ist was schief gelaufen", error)
-    if delete == True:
+def error_handling(error, delete=True):
+    file_name = ERROR_FOLDER + "error.txt"
+    print(error)
+    time.sleep(5)
+    with codecs.open(file_name, "a", "utf-8") as f:
+        f.write(error + "\n")
+    if delete:
         delete_file()
-    raise(error)
 
 
 def success():
     delete_file()
-    messagebox.showinfo("Erfolg", "Das File konnte erfolgreich auf den Server geladen und umgewandelt werden")
+    time.sleep(5)
 
 
 def delete_file():
     os.remove(IMPORT_FOLDER + get_file_from_folder())
+
 
 def get_label(file_name):
     file_name = re.sub('[^a-zA-Z.]+', '', file_name)
